952f8fbb692992e3b13aff8e8e0c4a8633440c00
Kiksa05 <kiksastefan555@gmail.com>
2024-05-25 22:22:38 +0200

    git commit -m "Vzpostavitev baze #RIS-5 DONE; Time 1h"

--- src/main/java/com/example/EcoRide/Rental/dao/EbikeDao.java
+++ src/main/java/com/example/EcoRide/Rental/dao/EbikeDao.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-//komentar1........
+//so ;
 
 @Repository
 public interface EbikeDao extends JpaRepository<Ebike, Integer> {
